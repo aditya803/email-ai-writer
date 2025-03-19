94bc5e9e5d00cc34f2336fb4ea684d52720dac4b
Aditya Pandey <adityapan25@gmail.com>
2025-03-20 01:53:19 +0530

    Initial commit

---
+++ src/main/java/com/email/email_ai/EmailAiwriterApplication.java
@@ -0,0 +1,13 @@
+package com.email.email_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailAiwriterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailAiwriterApplication.class, args);
+	}
+
+}

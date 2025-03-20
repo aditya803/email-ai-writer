a6ed5876980ed57b46e6e3a7dffb3ebad0e45e58
Aditya Pandey <adityapan25@gmail.com>
2025-03-21 01:32:56 +0530

    adding webflux dependency and creating service layer

--- src/main/java/com/email/email_ai/app/EmailGeneratorService.java
+++ src/main/java/com/email/email_ai/app/EmailGeneratorService.java
@@ -4,15 +4,25 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class EmailGeneratorService {
 
+    private final WebClient webClient;
+    
     @Value("${gemini.api.url}")
     private String geminiApiUrl;
     
     @Value("${gemini.api.key}")
     private String geminiApiKey;
+
+    public EmailGeneratorService(WebClient webClient){
+        this.webClient = webClient;
+    }
 
     public String generateEmailReply(EmailRequest emailRequest){
         //Build the prompt
@@ -28,9 +38,31 @@ public class EmailGeneratorService {
         );
 
         //Do request and get response
+        String response = webClient.post()
+                .uri(geminiApiUrl+geminiApiKey)
+                .header("Content-type", "application/json")
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
         
-
         //Return the response
+        return extractResponseContent(response);
+    }
+
+    private String extractResponseContent(String response){
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode rootNode = mapper.readTree(response);
+            return rootNode.path("candidates")
+                    .get(0)
+                    .path("content")
+                    .path("parts")
+                    .get(0)
+                    .path("text")
+                    .asText();
+        } catch (Exception e) {
+            return "Error processing message: "+e.getMessage();    
+        }
     }
 
     private String buildPrompt(EmailRequest emailRequest){
